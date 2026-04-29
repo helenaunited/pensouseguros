e5c7553dc61cff2c21def9204ee458e91464d4ef
helenaunited <helena.united@gmail.com>
2026-04-29 16:44:02 -0300

    Corrigindo palavra no código

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
-import { createClient } from '@libsql/client';
 
 const prismaClientSingleton = () => {
   if (process.env.TURSO_DATABASE_URL && process.env.TURSO_AUTH_TOKEN) {
-    const libsql = createClient({
+    const adapter = new PrismaLibSql({
       url: process.env.TURSO_DATABASE_URL,
       authToken: process.env.TURSO_AUTH_TOKEN,
     });
-    const adapter = new PrismaLibSql(libsql);
     return new PrismaClient({ adapter });
   } else {
     // Pass datasourceUrl directly if no adapter is used
